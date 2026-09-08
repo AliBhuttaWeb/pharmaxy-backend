8e2ae3173cf0bfd12277fdc2e6d940240d179246
Muhammad Ali <muhammad.ali@epicmetry.com>
2026-09-08 21:03:00 +0500

    fix: fixed text

--- src/modules/branch-products/constants/messages.constants.ts
+++ src/modules/branch-products/constants/messages.constants.ts
@@ -15,6 +15,6 @@ export const MESSAGES = {
         PRODUCT_NOT_FOUND: 'Product not found.',
         PRODUCT_REQUIRED: 'Product details are required when no product is selected.',
         BATCH_NOT_FOUND: 'No batch found.',
-        BRANCH_ID_MISSING: "Bnrach is missing, seems like you did't select any branch.",
+        BRANCH_ID_MISSING: "Branch id is missing, seems like you did't select any branch.",
     },
 } as const;
